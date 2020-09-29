62fdcd2e05e1e17d5ba8d3a8dc1e086d3f0c2050
HoneyehYz <yazdizadehhoneyeh@yahoo.com>
2020-09-29 01:38:58 +0000

    URL shortening part1

--- express_server.js
+++ express_server.js
@@ -1,9 +1,24 @@
 const express = require("express");
 const app = express();
 const PORT = 8080;
-
 app.set("view engine", "ejs");
+const bodyParser = require("body-parser");
 
+
+
+function generateRandomString() {
+  var result           = '';
+  var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  var charactersLength = characters.length;
+  for ( var i = 0; i < 6; i++ ) {
+     result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+}
+console.log(generateRandomString());
+
+
+app.use(bodyParser.urlencoded({extended: true}));
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -12,6 +27,10 @@ const urlDatabase = {
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
@@ -25,7 +44,10 @@ app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
-
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
